f67031d4ebba3e91c225b178d44baacabd6219da
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2021-02-02 14:46:13 +0000

    I made one of the 2 working methods for a int type (vgPrice)

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("The Video Game Price is: ");
+    System.out.println(vgPrice(60, 40) + " dollars");
   }
+
+
+static int vgPrice(int A, int B){
+  int videoGamePrice =  A - B;
+  return videoGamePrice; 
+}
 }
