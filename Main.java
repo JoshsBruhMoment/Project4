630ba3a1193113e8fef081f0673b9c3e1d7ba3c3
JoshsBruhMoment <joshua.aldahondo@patelhs.org>
2021-02-04 13:46:11 +0000

    I changed the code to make it flow better and just have more to offer than just 1 scan statement.

--- Main.java
+++ Main.java
@@ -6,18 +6,23 @@ class Main {
     System.out.println("what is your video games name?");
     String name = scan.next();
     System.out.println(videoGameName(name));
-    System.out.println("The Video Game Price is: " + vgPrice(60, 40) + " dollars");
+    System.out.println("How much is the video game?");
+    int price = scan.nextInt();
+    System.out.println("How much do you have?");
+    int amount = scan.nextInt();
+
+    // This prints the statement that includes the vgPrice method
+    System.out.println("Your leftover amount is: " + vgPrice(amount, price) + " dollars");
   }
 
   static String videoGameName(String name) {
     String vgname = "your video games name is : " + name;
     return vgname;
-    //This should print the video game name
-
+    // This should print the video game name
   }
 
-  static int vgPrice(int A, int B) {
-    int videoGamePrice = A - B;
+  static int vgPrice(int amount, int price) {
+    int videoGamePrice = amount - price;
     return videoGamePrice;
     // this prints the video game price
   }
